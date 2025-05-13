9a40473231e008641f11863e982f3290b0dbf13d
shahrukhvmi <shahrukh.vmi@gmail.com>
2025-05-13 19:41:42 +0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import "../styles/globals.css";
-import "../styles/fonts.css";
-import "../styles/paymentpage.css";
-
+import "@/styles/globals.css";
+import "@/styles/fonts.css";
+import "@/styles/paymentpage.css";
 import queryClient from "@/utils/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast"; // <-- ✅ YEH IMPORT KARO
 
 export default function App({ Component, pageProps }) {
   return (
