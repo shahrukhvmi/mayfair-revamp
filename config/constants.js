388e2156c52b88b5e19f551768c13b432380bde4
EmadAkbar <emad.vmi@gmail.com>
2025-07-23 12:44:20 +0500

    chatbot updated 23 july with pusher with build

--- config/constants.js
+++ config/constants.js
@@ -1,13 +1,13 @@
 // CONSTANT FILE
 
-// export const app_url = " https://staging.mayfairweightlossclinic.co.uk/api";
+export const app_url = " https://staging.mayfairweightlossclinic.co.uk/api";
 // export const app_url = " http://192.168.1.186:8000/api";
-export const app_url = " http://localhost:8000/api";
+// export const app_url = " http://localhost:8000/api";
 // export const passwordlink = "http://localhost:3000/email-confirmation"
 export const passwordlink =
   "https://mayfair-revamp.netlify.app/email-confirmation";
 
-export const pusher_key = "db19d7e930487b8f7d2b";
-export const pusher_cluster = "ap1";
+export const pusher_key = "e12b8fa1646968e644f3";
+export const pusher_cluster = "ap2";
 
 export default { app_url };
