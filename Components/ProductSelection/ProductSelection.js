a034af4ad762d2fdcbaabbd55b279f748431518b
usmanwarind46 <usmanwarind.vmi@gmail.com>
2025-08-28 13:28:36 +0500

    changes

--- Components/ProductSelection/ProductSelection.js
+++ Components/ProductSelection/ProductSelection.js
@@ -137,7 +137,7 @@ const ProductSelection = ({ showProductSelection }) => {
       {isLoading ? (
         renderSkeletons()
       ) : (
-        <div className="w-full flex flex-col items-center justify-center px-4 py-2">
+        <div className="w-full flex flex-col items-center justify-center px-4 py-2 overflow-y-auto">
           <div className="w-full flex flex-col items-center justify-center gap-8">
             {/* ───── Reorder Treatments ───── */}
 
@@ -159,7 +159,7 @@ const ProductSelection = ({ showProductSelection }) => {
                     productData.products.filter(
                       (p) => p?.inventories?.[0]?.status === 1
                     ).length === 1
-                      ? "justify-start"
+                      ? "justify-center"
                       : "justify-center"
                   }`}
                 >
@@ -216,6 +216,7 @@ const ProductSelection = ({ showProductSelection }) => {
                 onClick={hanlePrevData}
                 label="Continue"
                 loading={isButtonLoading}
+                className={"px-5"}
               />
             </div>
           </div>
