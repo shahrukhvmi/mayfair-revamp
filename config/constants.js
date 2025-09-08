36f1c04a5312c73827a222e0329e625cd9687257
usmanwarind46 <usmanwarind.vmi@gmail.com>
2025-09-08 19:53:38 +0500

    changes

--- config/constants.js
+++ config/constants.js
@@ -1,10 +1,11 @@
 // CONSTANT FILE
 
-export const app_url = "https://app.mayfairweightlossclinic.co.uk/api/";
-// export const app_url = " http://192.168.1.186:8000/api";
+// export const app_url = "https://app.mayfairweightlossclinic.co.uk/api/";
+export const app_url = " http://192.168.1.194:7000/api";
 // export const passwordlink = "http://localhost:3000/email-confirmation"
 export const passwordlink =
   "https://mayfairweightlossclinic.co.uk/start-consultation/email-confirmation";
-export const meta_url = "https://mayfairweightlossclinic.co.uk/start-consultation/";
+export const meta_url =
+  "https://mayfairweightlossclinic.co.uk/start-consultation/";
 
 export default { app_url };
