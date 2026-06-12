296ab9c238f6f1d61e6b310d5ac2038c89ec7842
shahrukhvmi <shahrukh.vmi@gmail.com>
2026-06-12 11:58:08 +0500

    up

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
             j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5GF4J86V');`
+            f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5GF4J86V');`,
           }}
         />
 
@@ -29,7 +29,37 @@ export default function Document() {
               s.parentNode.insertBefore(t,s)
             }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '377312261731717');
-            fbq('track', 'PageView');`
+            fbq('track', 'PageView');`,
+          }}
+        />
+
+        {/* CustomerLabs Tag script   */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(t,e,r,c,a,n,s){
+                t.ClAnalyticsObject=a,
+                t[a]=t[a]||[],
+                t[a].methods=["trackSubmit","trackClick","pageview","identify","track","trackConsent"],
+                t[a].factory=function(e){
+                  return function(){
+                    var r=Array.prototype.slice.call(arguments);
+                    return r.unshift(e),t[a].push(r),t[a]
+                  }
+                };
+                for(var i=0;i<t[a].methods.length;i++){
+                  var o=t[a].methods[i];
+                  t[a][o]=t[a].factory(o)
+                }
+                n=e.createElement(r),
+                s=e.getElementsByTagName(r)[0],
+                n.async=1,
+                n.crossOrigin="anonymous",
+                n.src=c,
+                s.parentNode.insertBefore(n,s)
+              }(window,document,"script","https://cdn.js.customerlabs.co/cl8600v4rvwsri.js","_cl");
+              _cl.SNIPPET_VERSION="2.0.0";
+            `,
           }}
         />
       </Head>
